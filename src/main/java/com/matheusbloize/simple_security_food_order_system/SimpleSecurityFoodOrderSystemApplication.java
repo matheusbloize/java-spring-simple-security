6f0f254c3be72d49f35ae5ce8d5e7a751fe4ffd6
Matheus Bloize <mbloizepd@gmail.com>
2025-02-28 11:12:41 -0300

    feat: start project

---
+++ src/main/java/com/matheusbloize/simple_security_food_order_system/SimpleSecurityFoodOrderSystemApplication.java
@@ -0,0 +1,13 @@
+package com.matheusbloize.simple_security_food_order_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSecurityFoodOrderSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleSecurityFoodOrderSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matheusbloize/simple_security_food_order_system/SimpleSecurityFoodOrderSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheusbloize.simple_security_food_order_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleSecurityFoodOrderSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
