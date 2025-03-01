a44d1afb5932a71d5e69c1de1ee82f1791474144
Matheus Bloize <mbloizepd@gmail.com>
2025-03-01 14:11:11 -0300

    feat: add status field to OrderDto

--- src/main/java/com/matheusbloize/simple_security_food_order_system/dtos/OrderDto.java
+++ src/main/java/com/matheusbloize/simple_security_food_order_system/dtos/OrderDto.java
@@ -3,8 +3,15 @@ package com.matheusbloize.simple_security_food_order_system.dtos;
 import java.util.List;
 import java.util.UUID;
 
+import com.matheusbloize.simple_security_food_order_system.enums.OrderStatus;
+
 import jakarta.validation.constraints.NotNull;
 
-public record OrderDto(@NotNull List<OrderProductDto> items, @NotNull UUID userId) {
+public record OrderDto(@NotNull List<OrderProductDto> items, @NotNull UUID userId, OrderStatus status) {
 
+    public OrderDto(@NotNull List<OrderProductDto> items, @NotNull UUID userId, OrderStatus status) {
+        this.items = items;
+        this.userId = userId;
+        this.status = status != null ? status : OrderStatus.CREATED;
+    }
 }
