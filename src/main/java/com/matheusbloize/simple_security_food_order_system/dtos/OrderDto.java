645da316284015d6da242886d86e7870a4e9df0c
Matheus Bloize <mbloizepd@gmail.com>
2025-03-01 12:05:13 -0300

    fix: change order dto items list to OrderProductDto

--- src/main/java/com/matheusbloize/simple_security_food_order_system/dtos/OrderDto.java
+++ src/main/java/com/matheusbloize/simple_security_food_order_system/dtos/OrderDto.java
@@ -3,10 +3,8 @@ package com.matheusbloize.simple_security_food_order_system.dtos;
 import java.util.List;
 import java.util.UUID;
 
-import com.matheusbloize.simple_security_food_order_system.models.OrderProduct;
-
 import jakarta.validation.constraints.NotNull;
 
-public record OrderDto(@NotNull List<OrderProduct> items, @NotNull UUID userId) {
+public record OrderDto(@NotNull List<OrderProductDto> items, @NotNull UUID userId) {
 
 }
