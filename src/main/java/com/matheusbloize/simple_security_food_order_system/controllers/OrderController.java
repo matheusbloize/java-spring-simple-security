0ee73a3d5662f20330a66a41224570a7ccbecf46
Matheus Bloize <mbloizepd@gmail.com>
2025-03-02 09:54:33 -0300

    feat: create role restrictions

--- src/main/java/com/matheusbloize/simple_security_food_order_system/controllers/OrderController.java
+++ src/main/java/com/matheusbloize/simple_security_food_order_system/controllers/OrderController.java
@@ -1,6 +1,7 @@
 package com.matheusbloize.simple_security_food_order_system.controllers;
 
 import java.math.BigDecimal;
+import java.security.Principal;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -9,6 +10,7 @@ import java.util.UUID;
 import org.springframework.beans.BeanUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.matheusbloize.simple_security_food_order_system.dtos.OrderDto;
 import com.matheusbloize.simple_security_food_order_system.enums.OrderStatus;
+import com.matheusbloize.simple_security_food_order_system.enums.RoleType;
 import com.matheusbloize.simple_security_food_order_system.models.Order;
 import com.matheusbloize.simple_security_food_order_system.models.OrderProduct;
 import com.matheusbloize.simple_security_food_order_system.models.Product;
@@ -46,13 +49,15 @@ public class OrderController {
         this.orderProductService = orderProductService;
     }
 
+    @PreAuthorize(value = "hasRole('ROLE_ADMIN')")
     @GetMapping
     ResponseEntity<List<Order>> listAll() {
         return ResponseEntity.status(HttpStatus.OK).body(orderService.listAll());
     }
 
+    @PreAuthorize(value = "hasRole('ROLE_USER')")
     @PostMapping
-    ResponseEntity<Object> save(@RequestBody @Valid OrderDto orderDto) {
+    ResponseEntity<Object> save(@RequestBody @Valid OrderDto orderDto, Principal principal) {
         Optional<User> userOpt = userService.findById(orderDto.userId());
         if (!userOpt.isPresent()) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User Not Found.");
@@ -89,24 +94,43 @@ public class OrderController {
             orderProd.setOrder(order);
             orderProductService.save(orderProd);
         });
-        return findById(order.getId());
+        return findById(order.getId(), principal);
     }
 
+    @PreAuthorize(value = "hasAnyRole('ROLE_USER', 'ROLE_ADMIN')")
     @GetMapping("/{id}")
-    ResponseEntity<Object> findById(@PathVariable(value = "id") UUID id) {
+    ResponseEntity<Object> findById(@PathVariable(value = "id") UUID id, Principal principal) {
         Optional<Order> orderOpt = orderService.findById(id);
         if (!orderOpt.isPresent()) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Order Not Found.");
+        }
+        Optional<User> userOpt = userService.findByName(principal.getName());
+        if (!userOpt.isPresent()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User Not Found.");
+        }
+        var isAdmin = userOpt.get().getAuthorities().stream().map(role -> role.getAuthority() == RoleType.ROLE_ADMIN.toString());
+        if (!isAdmin.anyMatch(t -> t) && orderOpt.get().getUser().getName() != userOpt.get().getName()) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body("This Order Isn't Yours.");
         }
         return ResponseEntity.status(HttpStatus.OK).body(orderOpt.get());
     }
 
+    @PreAuthorize(value = "hasAnyRole('ROLE_USER', 'ROLE_ADMIN')")
     @PutMapping("/{id}")
-    ResponseEntity<Object> update(@PathVariable(value = "id") UUID id, @RequestBody @Valid OrderDto orderDto) {
+    ResponseEntity<Object> update(@PathVariable(value = "id") UUID id, @RequestBody @Valid OrderDto orderDto, Principal principal) {
         Optional<Order> orderOpt = orderService.findById(id);
         if (!orderOpt.isPresent()) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Order Not Found.");
         }
+        Optional<User> userOpt = userService.findByName(principal.getName());
+        if (!userOpt.isPresent()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User Not Found.");
+        }
+        var isAdmin = userOpt.get().getAuthorities().stream().map(role -> role.getAuthority() == RoleType.ROLE_ADMIN.toString());
+        if (!isAdmin.anyMatch(t -> t) && orderOpt.get().getUser().getName() != userOpt.get().getName()) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body("This Order Isn't Yours.");
+        }
+
         Order order = new Order();
         BeanUtils.copyProperties(orderOpt.get(), order);
         BeanUtils.copyProperties(orderDto, order);
@@ -115,11 +139,21 @@ public class OrderController {
         return ResponseEntity.status(HttpStatus.OK).body(orderService.save(order));
     }
 
+    @PreAuthorize(value = "hasAnyRole('ROLE_USER', 'ROLE_ADMIN')")
     @DeleteMapping("/{id}")
-    ResponseEntity<Object> delete(@PathVariable(value = "id") UUID id) {
+    ResponseEntity<Object> delete(@PathVariable(value = "id") UUID id, Principal principal) {
         Optional<Order> orderOpt = orderService.findById(id);
         if (!orderOpt.isPresent()) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Order Not Found.");
+        }
+
+        Optional<User> userOpt = userService.findByName(principal.getName());
+        if (!userOpt.isPresent()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User Not Found.");
+        }
+        var isAdmin = userOpt.get().getAuthorities().stream().map(role -> role.getAuthority() == RoleType.ROLE_ADMIN.toString());
+        if (!isAdmin.anyMatch(t -> t) && orderOpt.get().getUser().getName() != userOpt.get().getName()) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body("This Order Isn't Yours.");
         }
 
         List<OrderProduct> orderProducts = orderProductService.listAllWithOrderId(id);
